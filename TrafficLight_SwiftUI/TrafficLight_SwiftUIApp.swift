6915f588a3681267ef782866ce4a1ad520292dd7
Sergey Zakharov <sergey_sentybr@mail.ru>
2023-07-15 23:47:18 +0200

    Initial Commit

---
+++ TrafficLight_SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight_SwiftUI
+//
+//  Created by Сергей Захаров on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
